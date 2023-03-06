33953b0759923de055d340f188110716b362038f
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2023-03-06 19:42:48 +0300

    Update vpc4.tf

--- vpc4.tf
+++ vpc4.tf
@@ -87,7 +87,7 @@ intra_subnet_tags = {
   ]
 }
 module "vpc_endpoints" {
-  source = "terraform-aws-vpc/modules/vpc-endpoints"
+  source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
 
   vpc_id             = module.vpc.vpc_id
   security_group_ids = [module.sg.vpc.id]
