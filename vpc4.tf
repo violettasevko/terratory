3d7b2884e869e4081bbb97f1f8acf0f7016cfd2a
Violetta Sevko <45388681+violettasevko@users.noreply.github.com>
2023-03-06 19:58:29 +0300

    Update vpc4.tf

--- vpc4.tf
+++ vpc4.tf
@@ -90,7 +90,7 @@ module "vpc_endpoints" {
   source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
 
   vpc_id             = module.vpc.vpc_id
-  security_group_ids = module.sg.id
+  security_group_ids = [module.sg.security_group_id]
 
   endpoints = {
     s3 = {
